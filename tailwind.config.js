780b3e008a032b1cdc111bf8608213db330a08a3
Abdul Aziz <abdulazizmishabhudin@gmail.com>
2025-01-15 13:46:12 +0700

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js, jsx"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#2C2C2C",
+				secondary: "#0B894A",
+				neutral: "#EDEDED",
+				success: "#8BB69B",
+				warning: "#D0D053",
+				error: "#D06253",
+			},
+		},
+	},
+	plugins: [],
+};
